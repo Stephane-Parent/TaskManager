422b5d8a9b34432f4544d5e5ade04cb31bfc61fa
Stephane Parent <stephane.parent@goto.com>
2024-04-21 14:01:22 -0400

    simplify ordering algo

--- my-service/src/schemas/workOrder/utils.ts
+++ my-service/src/schemas/workOrder/utils.ts
@@ -1,28 +1,23 @@
 import { WorkOrder } from './model';
 
 export const getOrganisedWorkOrder = (workOrders: WorkOrder[]) => {
-  const normalizedTodayDate = normalizeDate(Date.parse(new Date().toISOString()));
-
   return workOrders.sort((workOrderA, workOrderB) => {
-    const daysFromNowOrderA = getDaysFromNow(normalizeDate(workOrderA.dueDate), normalizedTodayDate);
-    const daysFromNowOrderB = getDaysFromNow(normalizeDate(workOrderB.dueDate), normalizedTodayDate);
+    const daysFromNowOrderA = getNumbersOfDays(workOrderA.dueDate);
+    const daysFromNowOrderB = getNumbersOfDays(workOrderB.dueDate);
+
     if (daysFromNowOrderA === daysFromNowOrderB) {
       const priorityLevelOrderA = getPriorityLevel(workOrderA.priority);
       const priorityLevelOrderB = getPriorityLevel(workOrderB.priority);
       return priorityLevelOrderA === priorityLevelOrderB ? 0 : priorityLevelOrderA > priorityLevelOrderB ? 1 : -1;
     }
+
     return daysFromNowOrderA > daysFromNowOrderB ? 1 : -1;
   });
 };
 
-const ONE_DAY = 24 * 60 * 60 * 1000;
-const normalizeDate = (date: number) => {
-  return Math.floor(date / ONE_DAY) * ONE_DAY;
-};
-
-const getDaysFromNow = (orderDate: number, dateNow: number) => {
-  const diffDaysFromNow = Math.floor((orderDate - dateNow) / ONE_DAY);
-  return diffDaysFromNow;
+const getNumbersOfDays = (date: number) => {
+  const ONE_DAY = 24 * 60 * 60 * 1000;
+  return Math.floor(date / ONE_DAY);
 };
 
 const getPriorityLevel = (priority) => {
